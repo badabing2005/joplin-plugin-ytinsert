786e0ec88d05e75e557f308338a5b3119e7a4c7d
Bing <badabing2005@hotmail.com>
2024-01-27 08:58:15 -0500

    v1.0.0 release

---
+++ README.md
@@ -0,0 +1,39 @@
+# Insert Youtube Video Link
+
+[![Release](https://img.shields.io/github/v/release/badabing2005/joplin-plugin-ytinsert?style=flat-square)](https://github.com/badabing2005/joplin-plugin-ytinsert/releases/latest) [![MIT](https://img.shields.io/github/license/badabing2005/joplin-plugin-ytinsert?style=flat-square)](/LICENSE)
+
+> Joplin Plugin
+
+## Features
+
+This plugin inserts a youtube video link into the note from the selected text.
+The selected text can be a youtube video link or just the video id.
+If the selected text is empty, the clipboard text is used.
+The format of the inserted link is: (where 0I_OPQQiYVU is the id of the video)
+
+```text
+[![Video](https://img.youtube.com/vi/0I_OPQQiYVU/maxresdefault.jpg)](https://www.youtube.com/watch?v=0I_OPQQiYVU)
+```
+
+## Screenshot
+
+![Screenshot](/screenshot.png)
+
+## Installation
+
+### Manual
+
+1. Go to the [Releases](https://github.com/badabing2005/joplin-plugin-ytinsert/releases/latest) to download the plugin package `zip` file and unzip it.
+2. Open [Joplin](https://joplinapp.org/), go to **Tools > Options > Plugins**.
+3. Click **Manage your plugins > Install from file**, select the previously unzipped `jpl` file.
+4. Finally, restart the application.
+
+## Building
+
+The plugin is built using Webpack, which creates the compiled code in `/dist`. A JPL archive will also be created in `/publish`, which can be used to distribute the plugin.
+
+To build the plugin, simply run `npm run dist`.
+
+## License
+
+[MIT](./LICENSE) license
---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import joplin from 'api';
+import { MenuItemLocation, ToolbarButtonLocation } from 'api/types';
+
+// This plugin inserts a youtube video link into the note from the selected text.
+// The selected text can be a youtube video link or just the video id.
+// If the selected text is empty, the clipboard text is used.
+// The format of the inserted link is: (where 0I_OPQQiYVU is the id of the video)
+// [![Video](https://img.youtube.com/vi/0I_OPQQiYVU/maxresdefault.jpg)](https://www.youtube.com/watch?v=0I_OPQQiYVU)
+
+joplin.plugins.register({
+    onStart: async function() {
+		// eslint-disable-next-line no-console
+		console.info('Insert Youtube Video  plugin started!');
+        await joplin.commands.register({
+            name: 'insertYoutubeVideo',
+            label: 'Insert YouTube Video',
+            iconName: 'fas fa-play-circle', // or use fa-video
+            execute: async () => {
+                let selectedText = await joplin.commands.execute('selectedText');
+				if (!selectedText) {
+					// Selection is empty, use clipboard text
+					selectedText = await joplin.clipboard.readText();
+				}
+				if (selectedText)
+				{
+                    const videoId = extractVideoId(selectedText);
+                    const markdown = `[![Video](https://img.youtube.com/vi/${videoId}/maxresdefault.jpg)](https://www.youtube.com/watch?v=${videoId})`;
+                    await joplin.commands.execute('replaceSelection', markdown);
+                }
+            },
+		});
+
+        // console.log('Creating toolbar button and menu item');
+        await joplin.views.toolbarButtons.create('myButton1', 'insertYoutubeVideo', ToolbarButtonLocation.EditorToolbar);
+		await joplin.views.menuItems.create('myMenuItem1', 'insertYoutubeVideo', MenuItemLocation.Tools, { accelerator: 'CmdOrCtrl+J' });
+    },
+});
+
+function extractVideoId(input: string): string {
+	const urlPattern = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?([^&]+)/;
+	const match = input.match(urlPattern);
+	return match ? match[1] : input;
+}
